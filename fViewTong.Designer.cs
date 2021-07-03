7839b75c37981e283a7c31b5ad1c062efe1272c2
Nguyễn Dương Hải <81065789+iamthedh7@users.noreply.github.com>
2021-07-03 19:09:59 +0700

    Add files via upload

---
+++ fViewTong.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace Giao_dien_quan_ly_thu_vien
+{
+    partial class fViewTong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.TaikhoanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.CapnhapToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.DangxuatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ThemsachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.XoasachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SuasachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LinhvucToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.LoaisachToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TacgiaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.HoadonToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.KhoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.NhaxuatbanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ThongkeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TaikhoanToolStripMenuItem,
+            this.SachToolStripMenuItem,
+            this.LinhvucToolStripMenuItem,
+            this.LoaisachToolStripMenuItem,
+            this.TacgiaToolStripMenuItem,
+            this.HoadonToolStripMenuItem,
+            this.KhoToolStripMenuItem,
+            this.NhaxuatbanToolStripMenuItem,
+            this.ThongkeToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1150, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // TaikhoanToolStripMenuItem
+            // 
+            this.TaikhoanToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CapnhapToolStripMenuItem,
+            this.DangxuatToolStripMenuItem});
+            this.TaikhoanToolStripMenuItem.Name = "TaikhoanToolStripMenuItem";
+            this.TaikhoanToolStripMenuItem.Size = new System.Drawing.Size(100, 24);
+            this.TaikhoanToolStripMenuItem.Text = "TÀI KHOẢN";
+            // 
+            // CapnhapToolStripMenuItem
+            // 
+            this.CapnhapToolStripMenuItem.Name = "CapnhapToolStripMenuItem";
+            this.CapnhapToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.CapnhapToolStripMenuItem.Text = "CẬP NHẬT";
+            this.CapnhapToolStripMenuItem.Click += new System.EventHandler(this.CapnhapToolStripMenuItem_Click);
+            // 
+            // DangxuatToolStripMenuItem
+            // 
+            this.DangxuatToolStripMenuItem.Name = "DangxuatToolStripMenuItem";
+            this.DangxuatToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.DangxuatToolStripMenuItem.Text = "ĐĂNG XUẤT";
+            this.DangxuatToolStripMenuItem.Click += new System.EventHandler(this.DangxuatToolStripMenuItem_Click);
+            // 
+            // SachToolStripMenuItem
+            // 
+            this.SachToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ThemsachToolStripMenuItem,
+            this.XoasachToolStripMenuItem,
+            this.SuasachToolStripMenuItem});
+            this.SachToolStripMenuItem.Name = "SachToolStripMenuItem";
+            this.SachToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.SachToolStripMenuItem.Text = "SÁCH";
+            // 
+            // ThemsachToolStripMenuItem
+            // 
+            this.ThemsachToolStripMenuItem.Name = "ThemsachToolStripMenuItem";
+            this.ThemsachToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.ThemsachToolStripMenuItem.Text = "THÊM SÁCH";
+            this.ThemsachToolStripMenuItem.Click += new System.EventHandler(this.ThemsachToolStripMenuItem_Click);
+            // 
+            // XoasachToolStripMenuItem
+            // 
+            this.XoasachToolStripMenuItem.Name = "XoasachToolStripMenuItem";
+            this.XoasachToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.XoasachToolStripMenuItem.Text = "XÓA SÁCH";
+            this.XoasachToolStripMenuItem.Click += new System.EventHandler(this.XoasachToolStripMenuItem_Click);
+            // 
+            // SuasachToolStripMenuItem
+            // 
+            this.SuasachToolStripMenuItem.Name = "SuasachToolStripMenuItem";
+            this.SuasachToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.SuasachToolStripMenuItem.Text = "SỬA SÁCH";
+            this.SuasachToolStripMenuItem.Click += new System.EventHandler(this.SuasachToolStripMenuItem_Click);
+            // 
+            // LinhvucToolStripMenuItem
+            // 
+            this.LinhvucToolStripMenuItem.Name = "LinhvucToolStripMenuItem";
+            this.LinhvucToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.LinhvucToolStripMenuItem.Text = "LĨNH VỰC";
+            this.LinhvucToolStripMenuItem.Click += new System.EventHandler(this.LinhvucToolStripMenuItem_Click);
+            // 
+            // LoaisachToolStripMenuItem
+            // 
+            this.LoaisachToolStripMenuItem.Name = "LoaisachToolStripMenuItem";
+            this.LoaisachToolStripMenuItem.Size = new System.Drawing.Size(96, 24);
+            this.LoaisachToolStripMenuItem.Text = "LOẠI SÁCH";
+            this.LoaisachToolStripMenuItem.Click += new System.EventHandler(this.LoaisachToolStripMenuItem_Click);
+            // 
+            // TacgiaToolStripMenuItem
+            // 
+            this.TacgiaToolStripMenuItem.Name = "TacgiaToolStripMenuItem";
+            this.TacgiaToolStripMenuItem.Size = new System.Drawing.Size(77, 24);
+            this.TacgiaToolStripMenuItem.Text = "TÁC GIẢ";
+            this.TacgiaToolStripMenuItem.Click += new System.EventHandler(this.TacgiaToolStripMenuItem_Click);
+            // 
+            // HoadonToolStripMenuItem
+            // 
+            this.HoadonToolStripMenuItem.Name = "HoadonToolStripMenuItem";
+            this.HoadonToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.HoadonToolStripMenuItem.Text = "HÓA ĐƠN";
+            this.HoadonToolStripMenuItem.Click += new System.EventHandler(this.HoadonToolStripMenuItem_Click);
+            // 
+            // KhoToolStripMenuItem
+            // 
+            this.KhoToolStripMenuItem.Name = "KhoToolStripMenuItem";
+            this.KhoToolStripMenuItem.Size = new System.Drawing.Size(54, 24);
+            this.KhoToolStripMenuItem.Text = "KHO";
+            this.KhoToolStripMenuItem.Click += new System.EventHandler(this.KhoToolStripMenuItem_Click);
+            // 
+            // NhaxuatbanToolStripMenuItem
+            // 
+            this.NhaxuatbanToolStripMenuItem.Name = "NhaxuatbanToolStripMenuItem";
+            this.NhaxuatbanToolStripMenuItem.Size = new System.Drawing.Size(130, 24);
+            this.NhaxuatbanToolStripMenuItem.Text = "NHÀ XUẤT BẢN";
+            this.NhaxuatbanToolStripMenuItem.Click += new System.EventHandler(this.NhaxuatbanToolStripMenuItem_Click);
+            // 
+            // ThongkeToolStripMenuItem
+            // 
+            this.ThongkeToolStripMenuItem.Name = "ThongkeToolStripMenuItem";
+            this.ThongkeToolStripMenuItem.Size = new System.Drawing.Size(95, 24);
+            this.ThongkeToolStripMenuItem.Text = "THỐNG KÊ";
+            this.ThongkeToolStripMenuItem.Click += new System.EventHandler(this.ThongkeToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Location = new System.Drawing.Point(12, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1126, 477);
+            this.panel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(1126, 477);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // fViewTong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1150, 520);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "fViewTong";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem TaikhoanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SachToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem LinhvucToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem LoaisachToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TacgiaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem HoadonToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem CapnhapToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem DangxuatToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ToolStripMenuItem ThemsachToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem XoasachToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem SuasachToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem KhoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem NhaxuatbanToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ThongkeToolStripMenuItem;
+    }
+}
